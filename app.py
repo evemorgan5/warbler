436d55e0d20295d70564c220ab5488f05152b5f9
Sara Heath <saramaheath@gmail.com>
2022-06-22 16:30:24 -0500

    updated show.html to include bio location and header image

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-# toolbar = DebugToolbarExtension(app)
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
